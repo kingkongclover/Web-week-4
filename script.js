991dafbfea58500a10e1a9350d2e630d0010618e
kingkongclover <anton.reijonen@gmail.com>
2024-09-30 21:51:21 +0300

    Viimeinkin voisi vaikka toimia :D

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 async function fetchData() {
+    // Variableen sarjat id:stä show-container ja alempana loopataan ja poistetaan laitetut sarjat
     var sarjat = document.querySelector('.show-container');
     while (sarjat.firstChild) {
         sarjat.removeChild(sarjat.firstChild);
     }
-    // Haetaan tiedot data muuttujaan
+    // Otetaan muuttujaan search talteen käyttäjän input
     let search = document.getElementById("input-show").value;
     console.log(search)
+    // Haetaan tiedot data muuttujaan tvURL välimuuttuja, johon voidaan laittaa userinput
     const tvURL = `https://api.tvmaze.com/search/shows?q=${search}`
     const response = await fetch(tvURL);
     const data = await response.json();
@@ -14,17 +16,20 @@ async function fetchData() {
 
     const containerDiv = document.querySelector('.show-container')
 
+    // Loopataan kaikki sarjat läpi yksitellen
+
     data.forEach(show => {
         const img = show.show.image.medium;
         const name = show.show.name;
         const summary = show.show.summary;
         const row = document.createElement('div')
-        row.classList.add('show-data')
+        // Annetaan row muuttujalle luodulle div:lle luokka show-data (Pelastus ei meinannut mennä läpi codegrade :D)
+        row.setAttribute("class", "show-data")
         console.log(img);
 
-
+        // Annetaan rivin sisälle kuva, nimi ja summary sarjasta
         row.innerHTML = `<img src=${img}><div class="show-info"><h1>${name}</h1>${summary}</div>`
-
+        // Lisätään rivi show-container div:in sisään
         containerDiv.appendChild(row);
     });
 }
